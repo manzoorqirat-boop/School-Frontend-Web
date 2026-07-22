afab2fd2495f38a4281891234a025f044793308e
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-07-23 04:03:05 +0530

    Update auth.tsx

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState, useCallback } from 'react';
+ React, { createContext, useContext, useEffect, useState, useCallback } from 'react';
 import { API, SessionUser } from './api';
 
 type AuthState = {
